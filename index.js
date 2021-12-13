da4af67c93bafd2207cb8da9f4032de074e5f14f
Md Shariful islam <shar51338@gmail.com git config --global user.name shariff61>
2021-12-13 21:02:08 +0200

    finsih pin-generate

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const generateButton = document.querySelector(".generate-btn");
 const formControl = document.querySelector(".form-control1");
 const calcFormControl = document.querySelector(".form-control2");
 const submitButton = document.querySelector(".submit-btn");
-// const notifySide = document.querySelectorAll(".notify");
 
-// random side
-// const formRandom = Math.trunc(Math.random() * 9999) + 1;
-
+// Random number
 generateButton.addEventListener("click", function () {
   const formRandom = Math.round(Math.random() * 9999) + 1;
 
@@ -36,22 +33,5 @@ submitButton.addEventListener("click", function () {
     for (let i = 0; i < twoNotify.length; i++) {
       twoNotify[i].style.display = "block";
     }
-    // document.querySelectorAll(".notify").style.display = "block";
   }
 });
-
-////////////////////////////////////////////////////////////////////////////
-
-// for (let i = 0; i < li.length; i++) {
-//   li[i].addEventListener("click", function () {
-//     if (this.innerText == "=") {
-//       result.value = eval(result.value);
-//     } else {
-//       if (this.innerText == "AC") {
-//         result.value = "";
-//       } else {
-//         result.value += this.innerText;
-//       }
-//     }
-//   });
-// }
